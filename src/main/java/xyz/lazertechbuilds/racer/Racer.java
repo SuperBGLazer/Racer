492d42d683d5afacceed96711deb9d9bc93f7ca3
SuperBGLazer <superlazer@icloud.com>
2021-05-15 20:11:44 -0500

    Initial commit

---
+++ src/main/java/xyz/lazertechbuilds/racer/Racer.java
@@ -0,0 +1,17 @@
+package xyz.lazertechbuilds.racer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Racer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
